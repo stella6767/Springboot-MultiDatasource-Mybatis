0d28b69e60f1b93b85e6338a99a839992304640d
강민규 <alsrb9434@gmail.com>
2021-08-18 18:35:53 +0900

    test patient

--- src/main/java/net/lunalabs/central/domain/oracle/Patient2.java
+++ src/main/java/net/lunalabs/central/domain/oracle/Patient2.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Patient {
+public class Patient2 {
 	
 	private int pid;
 	private String name;
--- src/main/java/net/lunalabs/central/web/dto/CMRespDto.java
+++ src/main/java/net/lunalabs/central/web/dto/CMRespDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class CMRespDto<T> {
+public class CMRespDto<T> { //Front 서버에 보내줄 공통 DTO
     private int statusCode;
     private String msg;
     private T data;
