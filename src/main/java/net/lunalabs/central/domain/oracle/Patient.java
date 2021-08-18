673781475bdec0a9f93f720352b719962cabd8fc
winix <winix@rigel.kr>
2021-08-18 19:09:10 +0900

    patient2 -> patient

---
+++ src/main/java/net/lunalabs/central/domain/oracle/Patient.java
@@ -0,0 +1,18 @@
+package net.lunalabs.central.domain.oracle;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Patient {
+	
+	private int pid;
+	private String name;
+	private String gender;
+	private int height;
+	private int weight;
+
+}
--- src/main/java/net/lunalabs/central/mapper/oracle/PatientMapper.java
+++ src/main/java/net/lunalabs/central/mapper/oracle/PatientMapper.java
@@ -3,17 +3,17 @@ package net.lunalabs.central.mapper.oracle;
 import java.util.List;
 
 import net.lunalabs.central.config.db.OracleConnMapper;
-import net.lunalabs.central.domain.oracle.Patient2;
+import net.lunalabs.central.domain.oracle.Patient;
 
 @OracleConnMapper("OraclePatientMapper")
 public interface PatientMapper {
 	
 	
-	public void save(Patient2 patient); 
+	public void save(Patient patient); 
 	public void deleteById(int id);
-	public void update(Patient2 patient);
-	public List<Patient2> findAll();
-	public Patient2 findById(int id);	
+	public void update(Patient patient);
+	public List<Patient> findAll();
+	public Patient findById(int id);	
 	
 
 }
