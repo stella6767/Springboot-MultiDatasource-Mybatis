e741d110d128a4e263167d0cbb0c931ee76b806e
강민규 <alsrb9434@gmail.com>
2021-08-26 16:00:17 +0900

    some change

---
+++ src/main/java/net/lunalabs/central/web/MeasureDataController.java
@@ -0,0 +1,23 @@
+package net.lunalabs.central.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class MeasureDataController {
+	
+	//일단은 mysql 기준
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(MeasureDataController.class);
+
+	//public void 
+	
+	
+	
+	
+}
