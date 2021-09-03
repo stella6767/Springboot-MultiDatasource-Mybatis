91530eb8990127e945a6adc60ec4956da5a00ed3
강민규 <alsrb9434@gmail.com>
2021-09-03 11:44:16 +0900

    front sse event test function add

--- src/main/java/net/lunalabs/central/CentralApplication.java
+++ src/main/java/net/lunalabs/central/CentralApplication.java
@@ -3,7 +3,9 @@ package net.lunalabs.central;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CentralApplication {
 
